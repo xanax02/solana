a6d7de93fdd4294ffbe2700965261a81086e755e
abhay <abhaytxanax@gmail.com>
2023-03-13 20:22:21 +0530

    debugged

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,8 @@ Dotenv.config();
 //         console.error(error);
 //     })
 
+
+// Initializing keyPair from envFile
 function initializeKeypair(): web3.Keypair {
     const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
     const secretKey = Uint8Array.from(secret);
@@ -22,16 +24,21 @@ function initializeKeypair(): web3.Keypair {
     return keypairFromSecret;
 }
 
+// Main Function
 async function main() {
     const payer = initializeKeypair(); 
     const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
-    await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL*2);
+    await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL*1);
     await pingProgram(connection, payer);
 }
 
+
+//Program Addresses
 const PROGRAM_ADDRESS = "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa";
 const PROGRAM_DATA_ADDRESS = "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod";
 
+
+// Ping Program
 async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {
     const transaction = new web3.Transaction();
 
@@ -62,6 +69,7 @@ async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {
     console.log(signature);
 }
 
+// Calling main Program
 main()
     .then(() => {
         console.log("Successfully finished")
