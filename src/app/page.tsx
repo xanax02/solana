aa094a3e5080db1dd0f11e4f5c50bbf35c3a6f1d
abhay <abhaytxanax@gmail.com>
2024-03-27 00:33:44 +0530

    connecting wallet to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-import { NextPage } from "next";
-import { FC, ReactNode } from "react";
+'use client'
+
+import { useEffect, useState } from "react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 export default function Home() {
+
+  const [balance, setBalance] = useState(0);
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  console.log("balance", balance)
+
+  useEffect(() => {
+    if(!publicKey || !connection) return;
+
+    connection.onAccountChange(
+      publicKey,
+      (updatedAccountInfo) => setBalance(updatedAccountInfo.lamports / LAMPORTS_PER_SOL),
+      "confirmed"
+    )
+
+    connection.getAccountInfo(publicKey).then(info =>{
+      if(info) setBalance(info.lamports);
+    })
+
+  }, [connection, publicKey])
+
   return (
     <main>
       <h1>Hello world</h1>
+      <p>{publicKey ? `Balance is ${balance / LAMPORTS_PER_SOL} sol` : ""}</p>
     </main>
   );
 }
