6b76ea52572ec0e82430ecd47313c53ef3ccb263
abhay <abhaytxanax@gmail.com>
2024-03-28 01:27:07 +0530

    connection-wallet-modal provider

---
+++ src/components/provider/Provider.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+export default function({children}: {children: ReactNode}) {
+
+    const endpoint = clusterApiUrl("devnet");
+    const wallets = useMemo(()=> [], [])
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets}>
+                <WalletModalProvider>
+                    {children}
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
