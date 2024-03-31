14dd945e7557b131dd4a96dccd6faf5a967bcaf2
abhay <abhaytxanax@gmail.com>
2024-03-31 15:05:05 +0530

    creating buffer

--- index.ts
+++ index.ts
@@ -5,3 +5,8 @@ const equipPlayerSchema = borsh.struct([
     borsh.u16('playerId'),
     borsh.u256('itemId')
 ])
+
+const buffer = Buffer.alloc(1000);
+equipPlayerSchema.encode({variant: 2, playerId: 1435, itemId: 737498}, buffer);
+
+const instructionBuffer = buffer.slice(0, equipPlayerSchema.getSpan(buffer));
