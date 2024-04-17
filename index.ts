6704c69c4a4c9bf9c09bb490d296cf8f3d843111
abhay <abhaytxanax@gmail.com>
2024-04-17 21:18:34 +0530

    multiple item per user pda

--- index.ts
+++ index.ts
@@ -13,3 +13,10 @@ const [pdaUserSpecific, bumpUserSpecific] = await findProgramAddresss(
   [PublicKey.toBuffer()],
   PROGRAM_ID
 );
+
+// program with multiple data items per user
+// seed can be address and title for a note taking app
+const [pdaItems, bumpItems] = await findProgramAddresss(
+  [PublicKey.toBuffer(), Buffer.from("Some_Title")],
+  PROGRAM_ID
+);
