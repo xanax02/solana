17b3f98d6a65c9acb3d21475269729d8d18bcfb2
abhay <abhaytxanax@gmail.com>
2024-04-19 04:05:01 +0530

    fetching data from chain

--- src/components/movieReviews/MovieReviews.tsx
+++ src/components/movieReviews/MovieReviews.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import { useConnection } from "@solana/wallet-adapter-react";
 import * as web3 from "@solana/web3.js";
@@ -22,6 +24,7 @@ export default function () {
         }
       });
       setMovies(movies);
+      console.log(movies);
     };
 
     fetchMovies();
@@ -30,23 +33,18 @@ export default function () {
   return (
     <div className="mt-4 mb-1">
       <p>Existing Reviews</p>
-      <div className="border-2 border-white px-10 py-4">
-        <h2 className="uppercase font-medium">This is the heading</h2>
-        <p>
-          Ultrices sagittis orci a scelerisque purus semper. Arcu risus quis
-          varius quam quisque id. Nam aliquam sem et tortor consequat. Amet
-          massa vitae tortor condimentum lacinia quis vel eros. Enim lobortis
-          scelerisque fermentum dui faucibus in. Fusce id velit ut tortor
-          pretium viverra suspendisse potenti nullam. At elementum eu facilisis
-          sed odio morbi. Enim nec dui nunc mattis enim ut tellus elementum.
-          Nascetur ridiculus mus mauris vitae ultricies. Velit ut tortor pretium
-          viverra. Mattis ullamcorper velit sed ullamcorper morbi tincidunt
-          ornare massa eget. Pellentesque id nibh tortor id aliquet lectus proin
-          nibh. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra
-          justo. Gravida in fermentum et sollicitudin. Tincidunt eget nullam non
-          nisi est sit amet.
-        </p>
-      </div>
+
+      {movies.map((movie, index) => {
+        return (
+          <div
+            key={index + 1}
+            className="border-2 border-white px-10 py-4 max-w-[500px] mb-2 break-words"
+          >
+            <h2 className="uppercase font-medium">{movie.title}</h2>
+            <p>{movie.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
