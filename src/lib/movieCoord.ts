15c560c9767f6e74a3e52f65df67b750a230f463
abhay <abhaytxanax@gmail.com>
2024-04-24 03:41:17 +0530

    sorting

--- src/lib/movieCoord.ts
+++ src/lib/movieCoord.ts
@@ -7,13 +7,36 @@ const program_id = new PublicKey(MOVIE_REVIEW_PROGRAM_ID);
 export class MovieCoordinator {
   static accounts: PublicKey[] = [];
 
+  // fetches all the accounts from chain with pubkey and limited account data
   static async prefetchAccounts(connection: Connection) {
     const accounts = await connection.getProgramAccounts(program_id, {
-      dataSlice: { offset: 0, length: 0 },
+      // for having title for sorting
+      dataSlice: { offset: 2, length: 18 },
     });
-    this.accounts = accounts.map((account) => account.pubkey);
+    // SORTING ACCOUNTS ACCORDING TO TITLE IN ALPHABETICAL ORDER
+    const accountsWithTitles = accounts.map((account) => account);
+
+    //
+    // could not do it as data is currupted.
+    //
+
+    // accountsWithTitles.sort((a, b) => {
+    //   const lengthA =
+    //     a.account.data.length >= 4 ? a.account.data.readUInt32LE(0) : 0;
+    //   const lengthB =
+    //     b.account.data.length >= 4 ? b.account.data.readUInt32LE(0) : 0;
+    //   console.log(lengthA, lengthB);
+    //   const dataA = a.account.data.slice(4, 4 + lengthA);
+    //   const dataB = b.account.data.slice(4, 4 + lengthB);
+    //   return dataA.compare(dataB);
+    // });
+
+    // setting class' accounts
+    this.accounts = accountsWithTitles.map((account) => account.pubkey);
   }
 
+  // slices the accounts array according to perPage and page number
+  // then with that sliced data of pubKeys fetches the accountdata
   static async fetchPage(
     connection: Connection,
     page: number,
